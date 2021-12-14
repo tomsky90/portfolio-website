f27764a4941cb894471017b44c50fab53ceae9f9
Tomasz K <kaszyckitmasz@interia.pl>
2021-12-14 22:19:07 +0000

    add about me + my skills sections

--- main.js
+++ main.js
@@ -3,28 +3,37 @@ const welcomeMessages = ['Hi, there!', 'My name is Tom', "I'm a Front-End develo
 const firstParagraph = document.querySelector('.welcom-txt__first-txt');
 const heading = document.querySelector('.welcom-txt h1');
 const secondParagraph = document.querySelector('.welcom-txt__second-txt');
-let wordIndex = 0;
-let textIndex = 0;
+let activeLetter = 0;
+let activeText = 0;
 
 const displayWelcomeMessages = () => {
-    if(textIndex === 0) {
-        firstParagraph.textContent += welcomeMessages[textIndex][wordIndex]
-    wordIndex++
+    
+    if(activeText === 0) {
+        firstParagraph.textContent += welcomeMessages[activeText][activeLetter]
+        activeLetter++
     }
     
-    if(wordIndex === welcomeMessages[textIndex].length) {
-        textIndex++;
-        wordIndex = 0;
+    if(activeLetter === welcomeMessages[activeText].length) {
+        
+        activeText++;
+        activeLetter = 0;
+        return setTimeout(displayWelcomeMessages, 1000)
+       
     }
-    if(textIndex === 1) {
-        heading.textContent += welcomeMessages[textIndex][wordIndex]
-        wordIndex++
+    if(activeText === 1) {
+        
+        heading.textContent += welcomeMessages[activeText][activeLetter]
+        activeLetter++
     }
-    if(textIndex === 2) {
-        secondParagraph.textContent += welcomeMessages[textIndex][wordIndex]
-        wordIndex++
+    if(activeText === 2) {
+        secondParagraph.textContent += welcomeMessages[activeText][activeLetter]
+        activeLetter++
+        if(activeLetter === welcomeMessages[activeText].length) {
+            return
+        }
     }
-    setTimeout(() => {displayWelcomeMessages()}, 150)
+
+    setTimeout(displayWelcomeMessages, 80)
 }
 
 const animateCursor = () => {
@@ -32,5 +41,5 @@ const animateCursor = () => {
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    displayWelcomeMessages()
+   setTimeout(displayWelcomeMessages, 1000) 
 });
