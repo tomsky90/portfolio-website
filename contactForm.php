4360391412f43d28e3300dfcb2f14405b887e39e
Tomasz K <kaszyckitmasz@interia.pl>
2021-12-29 01:40:24 +0000

    add php contact form file

---
+++ contactForm.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $emailFrom = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = "kaszyckitomasz@interia.pl";
+    $headers = "From: ".$emailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend")
+}
