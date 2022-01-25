bb44dac07bc6c6b500b69943ef3d6ea671a6d2d1
Tomasz K <kaszyckitmasz@interia.pl>
2022-01-25 22:53:26 +0000

    updaet form + header layout

--- main.js
+++ main.js
@@ -1,49 +1,4 @@
 
-// Header text animation
-
-const welcomeMessages = ['Hi, there!', 'My name is Tom', "I'm a Front-End developer pasionate about creaiting web applications"];
-
-const firstParagraph = document.querySelector('.welcom-txt__first-txt');
-const heading = document.querySelector('.welcom-txt h1');
-const secondParagraph = document.querySelector('.welcom-txt__second-txt');
-let activeLetter = 0;
-let activeText = 0;
-
-const displayWelcomeMessages = () => {
-    
-    if(activeText === 0) {
-        firstParagraph.textContent += welcomeMessages[activeText][activeLetter]
-        activeLetter++
-    }
-    
-    if(activeLetter === welcomeMessages[activeText].length) {
-        
-        activeText++;
-        activeLetter = 0;
-        return setTimeout(displayWelcomeMessages, 1000)
-       
-    }
-    if(activeText === 1) {
-        
-        heading.textContent += welcomeMessages[activeText][activeLetter]
-        activeLetter++
-    }
-    if(activeText === 2) {
-        secondParagraph.textContent += welcomeMessages[activeText][activeLetter]
-        activeLetter++
-        if(activeLetter === welcomeMessages[activeText].length) {
-            return
-        }
-    }
-
-    setTimeout(displayWelcomeMessages, 80)
-}
-
-document.addEventListener("DOMContentLoaded", function(event) {
-   setTimeout(displayWelcomeMessages, 1000) 
-});
-
-
 // section observer for projects section
 
 const imgsToAppear = document.querySelectorAll('.projects__img-container');
